f8f17a1e6c7cbf248eb8e2e167c2d3886ffb15bf
HyeonDeok Lee <hd1983@nablecomm.com>
2017-05-23 11:06:55 +0900

    callkit 지원을 위한 INStartAudioCallIntent value 추가

--- iphone/TitaniumKit/TitaniumKit/Sources/API/TiApp.m
+++ iphone/TitaniumKit/TitaniumKit/Sources/API/TiApp.m
@@ -21,6 +21,7 @@
 #import <CoreSpotlight/CoreSpotlight.h>
 #import <QuartzCore/QuartzCore.h>
 #import <libkern/OSAtomic.h>
+#import <Intents/Intents.h>
 
 TiApp *sharedApp;
 
@@ -1136,6 +1137,15 @@ TI_INLINE void waitForMemoryPanicCleared(); //WARNING: This must never be run on
 
   if ([userActivity userInfo] != nil) {
     [dict setObject:[userActivity userInfo] forKey:@"userInfo"];
+  }
+
+  if([TiUtils isIOS9OrGreater] && [[userActivity activityType] isEqualToString:@"INStartAudioCallIntent"]){
+      INInteraction *interaction = userActivity.interaction;
+      INStartAudioCallIntent *startAudioCallIntent = (INStartAudioCallIntent *)interaction.intent;
+      INPerson *contact = startAudioCallIntent.contacts[0];
+      INPersonHandle *personHandle = contact.personHandle;
+      NSString *phoneNumber = personHandle.value;
+      [dict setObject:phoneNumber forKey:@"INStartAudioCallIntentActivityValue"];
   }
 
   // Update launchOptions so that we send only expected values rather than NSUserActivity
