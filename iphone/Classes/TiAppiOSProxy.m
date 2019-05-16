b849027a356715bde09690c89ef25fcf1a5ab6a5
HyeonDeok Lee <hd1983@nablecomm.com>
2019-05-16 17:49:25 +0900

    [iOS] pushKit에 대한 등록을 모듈에서 할때 아이폰이 재부팅된 이후 정상적으로 voip push를 수신받지 못하는 문제가 있어 push kit 등록을 SDK에서 하도록 수정. - background mode 에 voip가 있을경우 push kit을 등록하도록 수정. - TiAppiOSProxy.m 에 push kit에 대한 처리 이벤트를 app으로 전달 할 수 있게 Observer등
    (cherry picked from commit 6650a5841def2ce92c465612e0f707e7b7f35b16)

--- iphone/Classes/TiAppiOSProxy.m
+++ iphone/Classes/TiAppiOSProxy.m
@@ -127,6 +127,15 @@
                                                object:nil];
   }
 
+  if ([TiUtils isIOS8OrGreater]) {
+    if ((count == 1) && [type isEqual:@"receiveAPNS"]) {
+      [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didReceiveVoipPushNotification:) name:@"receiveAPNS" object:nil];
+    }
+    if ((count == 1) && [type isEqual:@"registeVoipToken"]) {
+      [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didReceivePushCredentials:) name:@"registeVoipToken" object:nil];
+    }
+  }
+
   if ((count == 1) && [type isEqual:@"traitcollectionchange"]) {
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(didChangeTraitCollection:)
@@ -185,6 +194,15 @@
   }
   if ((count == 1) && [type isEqual:@"shortcutitemclick"]) {
     [[NSNotificationCenter defaultCenter] removeObserver:self name:kTiApplicationShortcut object:nil];
+  }
+
+  if ([TiUtils isIOS10OrGreater]) {
+    if ((count == 1) && [type isEqual:@"receiveAPNS"]) {
+      [[NSNotificationCenter defaultCenter] removeObserver:self name:@"receiveAPNS" object:nil];
+    }
+    if ((count == 1) && [type isEqual:@"registeVoipToken"]) {
+      [[NSNotificationCenter defaultCenter] removeObserver:self name:@"registeVoipToken" object:nil];
+    }
   }
   if ((count == 1) && [type isEqual:@"traitcollectionchange"]) {
     [[NSNotificationCenter defaultCenter] removeObserver:self name:kTiTraitCollectionChanged object:nil];
@@ -1083,6 +1101,15 @@
 {
   [self fireEvent:@"usernotificationsettings"
        withObject:[self formatUserNotificationSettings:(UIUserNotificationSettings *)[[note userInfo] valueForKey:@"userNotificationSettings"]]];
+}
+
+- (void)didReceiveVoipPushNotification:(NSNotification *)note
+{
+  [self fireEvent:@"receiveAPNS" withObject:[note userInfo]];
+}
+- (void)didReceivePushCredentials:(NSNotification *)note
+{
+  [self fireEvent:@"registeVoipToken" withObject:[note userInfo]];
 }
 
 #pragma mark Apple Watchkit notifications
